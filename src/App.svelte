a7ab12b4efe67f4376f0882c7ef127aab2088db6
JosseMontano <josemariazambranamontano123@gmail.com>
2023-09-10 17:58:26 -0400

    change state

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import CatsComponent from "./components/Cats.svelte";
+
+  let sectionCurrent = "Todos";
+
+  const handleChangeCat = (cat: string) => {
+    sectionCurrent = cat;
+  };
+</script>
+
+<main class="container__dad">
+  <div class="container__son">
+    <h1 class="title">Clothes Stores</h1>
+
+    <CatsComponent {handleChangeCat} {sectionCurrent} />
+
+    <div>
+      <p>Catch: {sectionCurrent}</p>
+    </div>
+  </div>
+</main>
+
+<style>
+  .container__dad {
+    display: flex;
+    justify-content: center;
+  }
+  .container__son {
+    width: 800px;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+  }
+  .title {
+    font-size: 32px;
+    font-weight: bold;
+    color: #4b2b7a;
+  }
+</style>
---
+++ src/components/Cats.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  export let handleChangeCat: (val: string) => void;
+  export let sectionCurrent: string;
+
+  //array btns
+  interface BtnSvelteType {
+    name: string;
+  }
+  const btnSvelte: BtnSvelteType[] = [
+    {
+      name: "Todos",
+    },
+    {
+      name: "Recomendados",
+    },
+    {
+      name: "Tradicional",
+    },
+    {
+      name: "Zapatos",
+    },
+  ];
+</script>
+
+<div>
+  <div class="container__section">
+    {#each btnSvelte as item}
+      <button
+        class={item.name == sectionCurrent ? "section--active" : ""}
+        on:click={() => handleChangeCat(item.name)}>{item.name}</button
+      >
+    {/each}
+  </div>
+</div>
+
+<style>
+  .container__section {
+    display: flex;
+    gap: 10px;
+    border-bottom: 1px solid #dcdcdc;
+  }
+
+  button {
+    padding: 10px;
+    cursor: pointer;
+    background-color: transparent;
+    border: none;
+    font-size: 20px;
+  }
+
+  .section--active {
+    background-color: #f2f2f2;
+    color: #4b2b7a;
+    border-bottom: 1px solid #4b2b7a;
+    font-weight: 600;
+  }
+</style>
